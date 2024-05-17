f7c459134178266084a0a999e8cf9911a500b010
Your Name <you@example.com>
2024-05-17 10:03:45 +0530

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import BannerImages from "@/components/BannerImages";
 export default async function Home() {
     return (
         <>
+            <h1>Welcome to grocery store</h1>
             <BannerImages />
         </>
     );
