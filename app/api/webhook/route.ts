43aa8548cc475c75bb37ab02db566fea0fd07ee4
Your Name <you@example.com>
2024-05-29 23:00:24 +0530

    modified:   app/api/webhook/route.ts

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -11,65 +11,67 @@ export async function POST(req: NextRequest) {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
     const signature = headers().get("stripe-signature") as string;
 
+    console.log(body, signature);
+
     try {
         event = stripe.webhooks.constructEvent(
             body,
             signature,
             process.env.STRIPE_WEBHOOK_KEY as string
         );
+
+        if (event.type === "checkout.session.completed") {
+            lineitems = await stripe.checkout.sessions.listLineItems(
+                event.data.object.id
+            );
+
+            lineitems = lineitems.data.map((item) => ({
+                quantity: item.quantity as number,
+                unit_amount: (item.price?.unit_amount as number) / 100,
+                amount_total: (item.amount_total as number) / 100,
+                description: item.description as string,
+            }));
+
+            const name = event.data.object.customer_details?.name as string;
+            const email = event.data.object.customer_email as string;
+            const line1 = event.data.object.shipping_details?.address
+                ?.line1 as string;
+            const line2 = event.data.object.shipping_details?.address
+                ?.line2 as string;
+            const city = event.data.object.shipping_details?.address
+                ?.city as string;
+            const state = event.data.object.shipping_details?.address
+                ?.state as string;
+            const country = event.data.object.shipping_details?.address
+                ?.country as string;
+            const pin = event.data.object.shipping_details?.address
+                ?.postal_code as string;
+            const total = (event.data.object.amount_total as number) / 100;
+
+            await addOrder({
+                name,
+                email,
+                line1,
+                line2,
+                city,
+                state,
+                pin,
+                country,
+                order: lineitems,
+                total,
+            });
+
+            await emptyCart(email);
+
+            return NextResponse.json({
+                status: 200,
+                message: "Successful webhook call",
+            });
+        }
     } catch (error) {
         return NextResponse.json({
             status: 500,
             message: "Could not create the webhook event",
-        });
-    }
-
-    if (event.type === "checkout.session.completed") {
-        lineitems = await stripe.checkout.sessions.listLineItems(
-            event.data.object.id
-        );
-
-        lineitems = lineitems.data.map((item) => ({
-            quantity: item.quantity as number,
-            unit_amount: (item.price?.unit_amount as number) / 100,
-            amount_total: (item.amount_total as number) / 100,
-            description: item.description as string,
-        }));
-
-        const name = event.data.object.customer_details?.name as string;
-        const email = event.data.object.customer_email as string;
-        const line1 = event.data.object.shipping_details?.address
-            ?.line1 as string;
-        const line2 = event.data.object.shipping_details?.address
-            ?.line2 as string;
-        const city = event.data.object.shipping_details?.address
-            ?.city as string;
-        const state = event.data.object.shipping_details?.address
-            ?.state as string;
-        const country = event.data.object.shipping_details?.address
-            ?.country as string;
-        const pin = event.data.object.shipping_details?.address
-            ?.postal_code as string;
-        const total = (event.data.object.amount_total as number) / 100;
-
-        await addOrder({
-            name,
-            email,
-            line1,
-            line2,
-            city,
-            state,
-            pin,
-            country,
-            order: lineitems,
-            total,
-        });
-
-        await emptyCart(email);
-
-        return NextResponse.json({
-            status: 200,
-            message: "Successful webhook call",
         });
     }
 }
