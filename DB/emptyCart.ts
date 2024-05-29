fcca9b99c0dee5f238587f91f6445e6e5563687d
Your Name <you@example.com>
2024-05-29 21:32:02 +0530

    modified:   DB/emptyCart.ts

--- DB/emptyCart.ts
+++ DB/emptyCart.ts
@@ -2,11 +2,14 @@
 
 import connectDB from "./connection";
 import { Orders } from "./models/Orders";
+import { revalidatePath } from "next/cache";
 
 export default async function emptyCart(email: string) {
     try {
         await connectDB();
         await Orders.deleteMany({ email });
+
+        revalidatePath("/mycart", "page");
     } catch (error) {
         console.log();
     }
