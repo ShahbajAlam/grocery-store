02a57c18d1b305de8ed34ff21104b58c9c99d6f6
Your Name <you@example.com>
2024-06-02 12:06:59 +0530

    modified:   app/api/webhook/route.ts 	modified:   package-lock.json 	modified:   package.json

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,11 +2,9 @@ import Stripe from "stripe";
 import { headers } from "next/headers";
 import addOrder from "@/DB/addOrder";
 import emptyCart from "@/DB/emptyCart";
-import { NextResponse } from "next/server";
-import { buffer } from "micro";
-import getRawBody from "raw-body";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
     let lineitems;
     let event: Stripe.Event;
     const body = await req.text();
@@ -20,53 +18,50 @@ export async function POST(req: Request) {
             signature,
             process.env.STRIPE_WEBHOOK_KEY as string
         );
+
         if (event.type === "checkout.session.completed") {
-            console.log(signature);
-            console.log(body);
+            lineitems = await stripe.checkout.sessions.listLineItems(
+                event.data.object.id
+            );
+
+            lineitems = lineitems.data.map((item) => ({
+                quantity: item.quantity as number,
+                unit_amount: (item.price?.unit_amount as number) / 100,
+                amount_total: (item.amount_total as number) / 100,
+                description: item.description as string,
+            }));
+
+            const name = event.data.object.customer_details?.name as string;
+            const email = event.data.object.customer_email as string;
+            const line1 = event.data.object.shipping_details?.address
+                ?.line1 as string;
+            const line2 = event.data.object.shipping_details?.address
+                ?.line2 as string;
+            const city = event.data.object.shipping_details?.address
+                ?.city as string;
+            const state = event.data.object.shipping_details?.address
+                ?.state as string;
+            const country = event.data.object.shipping_details?.address
+                ?.country as string;
+            const pin = event.data.object.shipping_details?.address
+                ?.postal_code as string;
+            const total = (event.data.object.amount_total as number) / 100;
+
+            await addOrder({
+                name,
+                email,
+                line1,
+                line2,
+                city,
+                state,
+                pin,
+                country,
+                order: lineitems,
+                total,
+            });
+
+            await emptyCart(email);
         }
-        // if (event.type === "checkout.session.completed") {
-        //     lineitems = await stripe.checkout.sessions.listLineItems(
-        //         event.data.object.id
-        //     );
-
-        //     lineitems = lineitems.data.map((item) => ({
-        //         quantity: item.quantity as number,
-        //         unit_amount: (item.price?.unit_amount as number) / 100,
-        //         amount_total: (item.amount_total as number) / 100,
-        //         description: item.description as string,
-        //     }));
-
-        //     const name = event.data.object.customer_details?.name as string;
-        //     const email = event.data.object.customer_email as string;
-        //     const line1 = event.data.object.shipping_details?.address
-        //         ?.line1 as string;
-        //     const line2 = event.data.object.shipping_details?.address
-        //         ?.line2 as string;
-        //     const city = event.data.object.shipping_details?.address
-        //         ?.city as string;
-        //     const state = event.data.object.shipping_details?.address
-        //         ?.state as string;
-        //     const country = event.data.object.shipping_details?.address
-        //         ?.country as string;
-        //     const pin = event.data.object.shipping_details?.address
-        //         ?.postal_code as string;
-        //     const total = (event.data.object.amount_total as number) / 100;
-
-        //     await addOrder({
-        //         name,
-        //         email,
-        //         line1,
-        //         line2,
-        //         city,
-        //         state,
-        //         pin,
-        //         country,
-        //         order: lineitems,
-        //         total,
-        //     });
-
-        //     await emptyCart(email);
-        // }
         return NextResponse.json({ message: "Webhook call is successful" });
     } catch (error) {
         if (error instanceof Error)
