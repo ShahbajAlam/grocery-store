62afe96cdeef95d2ec9c41cb70deda3178b341c1
Your Name <you@example.com>
2024-06-12 15:37:27 +0530

    modified:   components/AddToWishlistButton.tsx 	new file:   public/heart.png 	new file:   public/like.png

--- components/AddToWishlistButton.tsx
+++ components/AddToWishlistButton.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { ProductsProps } from "@/types";
-import { useWishlist } from "@/contexts/WishlistContext";
 import showToast from "@/utils/showToast";
+import { useWishlist } from "@/contexts/WishlistContext";
 
 export default function AddToWishlistButton({ item }: { item: ProductsProps }) {
     const data = useWishlist();
@@ -11,27 +11,29 @@ export default function AddToWishlistButton({ item }: { item: ProductsProps }) {
         return data?.products.some((item) => item._id === id);
     };
 
+    const handleAddToWishlistButton = () => {
+        if (isInWishlist(item._id)) {
+            data?.removeFromWishlist(item._id);
+            showToast({
+                type: "success",
+                message: `${item.name} is removed from wishlist`,
+            });
+        } else {
+            data?.addToWishlist(item);
+            showToast({
+                type: "success",
+                message: `${item.name} is added to wishlist`,
+            });
+        }
+    };
+
     return (
-        <span
-            role="button"
-            className={`text-3xl ${isInWishlist(item._id) ? "text-red-600" : "text-yellow-700"}`}
-            onClick={() => {
-                if (isInWishlist(item._id)) {
-                    data?.removeFromWishlist(item._id);
-                    showToast({
-                        type: "success",
-                        message: `${item.name} is removed from wishlist`,
-                    });
-                } else {
-                    data?.addToWishlist(item);
-                    showToast({
-                        type: "success",
-                        message: `${item.name} is added to wishlist`,
-                    });
-                }
-            }}
-        >
-            &#10084;
+        <span role="button" onClick={handleAddToWishlistButton}>
+            {isInWishlist(item._id) ? (
+                <img src="/heart.png" alt="heart icon" width={30} height={30} />
+            ) : (
+                <img src="/like.png" alt="heart icon" width={30} height={30} />
+            )}
         </span>
     );
 }
